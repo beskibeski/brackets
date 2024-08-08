e8905e3c9c5a6403c209f5a256a44faf92d2447a
unknown <benladen1917@inbox.ru>
2024-08-08 22:14:46 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const newBracketsConfig = bracketsConfig.map(element => element.join(''));
+    while (str.length) {
+  	  let newStrLength = str.length;
+  	  for(let key of newBracketsConfig) {
+    	  str = str.replace(key, '');
+      }
+      if (newStrLength === str.length) {
+        return false;
+      }
+    }
+  return true;
 }
